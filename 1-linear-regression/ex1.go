1317bdfa2e660b5d351601f9e79972279276d055
Stephen Mahon <stemahon@gmail.com>
2021-07-31 21:09:18 -0500

    init commit

---
+++ 1-linear-regression/ex1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Machine Learning Online Class - Exercise 1: Linear Regression
+
+// x refers to the population size in 10,000s
+// y refers to the profit in $10,000s
+
+func main() {
+	fmt.Printf("Running warmUpExercise ... \n")
+	fmt.Printf("5x5 Identity Matrix: \n")
+	warmup()
+}
