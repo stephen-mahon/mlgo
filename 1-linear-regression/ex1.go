86499f36b487600a20e595b00eef0e1d3dd3a24f
Stephen Mahon <stemahon@gmail.com>
2021-11-05 23:20:32 -0500

    added plotting func and straight line

--- 1-linear-regression/ex1.go
+++ 1-linear-regression/ex1.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"image/color"
 	"log"
 	"os"
 
@@ -23,7 +22,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not read %v: %v", fileName, err)
 	}
-	_ = xys
 
 	err = plotData("out.png", xys)
 	if err != nil {
@@ -31,24 +29,25 @@ func main() {
 	}
 }
 
-type xy struct{ x, y float64 }
+//type xy struct{ x, y float64 }
 
-func readData(path string) ([]xy, error) {
+func readData(path string) (plotter.XYs, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
 
-	var xys []xy
+	var xys plotter.XYs
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		var x, y float64
 		_, err := fmt.Sscanf(s.Text(), "%f,%f", &x, &y)
 		if err != nil {
 			log.Printf("discarding bad data point %q: %v", s.Text(), err)
+			continue
 		}
-		xys = append(xys, xy{x, y})
+		xys = append(xys, struct{ X, Y float64 }{x, y})
 	}
 	if err := s.Err(); err != nil {
 		return nil, fmt.Errorf("could not scan: %v", err)
@@ -56,27 +55,38 @@ func readData(path string) ([]xy, error) {
 	return xys, nil
 }
 
-func plotData(path string, xys []xy) error {
+func plotData(path string, xys plotter.XYs) error {
 	f, err := os.Create(path)
 	if err != nil {
 		return fmt.Errorf("could not create %s: %v", path, err)
 	}
 
 	p := plot.New()
-	wt, err := p.WriterTo(512, 512, "png")
-	if err != nil {
-		return fmt.Errorf("could not create writer: %v", err)
-	}
 
-	s, err := plotter.NewScatter(plotter.XYs{
-		{0, 0}, {0.5, 0.5}, {1, 1},
-	})
+	// create scatter with all data points
+	s, err := plotter.NewScatter(xys)
 	if err != nil {
 		return fmt.Errorf("could not create scatter : %v", err)
 	}
 	s.GlyphStyle.Shape = draw.CrossGlyph{}
-	s.GlyphStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
 	p.Add(s)
+
+	var x, c float64
+	x = 1.2
+	c = -3
+	// create fake linear regression restult
+	l, err := plotter.NewLine(plotter.XYs{
+		{3, 3*x + c}, {20, 20*x + c},
+	})
+	if err != nil {
+		return fmt.Errorf("could not create line: %v", err)
+	}
+	p.Add(l)
+
+	wt, err := p.WriterTo(512, 512, "png")
+	if err != nil {
+		return fmt.Errorf("could not create writer: %v", err)
+	}
 
 	_, err = wt.WriteTo(f)
 	if err != nil {
