61bc9bbe87172c833d46b4ae3f469e49de49f9b7
Stephen Mahon <stemahon@gmail.com>
2021-11-06 23:18:48 -0500

    added linear regression, cost, grad decent

--- 1-linear-regression/ex1.go
+++ 1-linear-regression/ex1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -16,8 +17,11 @@ import (
 // x refers to the population size in 10,000s
 // y refers to the profit in $10,000s
 var fileName = "ex1data1.txt"
+var iterations int
 
 func main() {
+	flag.IntVar(&iterations, "n", 1000, "number of iterations for linear regression")
+	flag.Parse()
 	xys, err := readData(fileName)
 	if err != nil {
 		log.Fatalf("could not read %v: %v", fileName, err)
@@ -71,9 +75,8 @@ func plotData(path string, xys plotter.XYs) error {
 	s.GlyphStyle.Shape = draw.CrossGlyph{}
 	p.Add(s)
 
-	var x, c float64
-	x = 1.2
-	c = -3
+	x, c := linearRegression(xys, 0.01)
+
 	// create fake linear regression restult
 	l, err := plotter.NewLine(plotter.XYs{
 		{3, 3*x + c}, {20, 20*x + c},
@@ -96,4 +99,46 @@ func plotData(path string, xys plotter.XYs) error {
 		return fmt.Errorf("could not close %s: %v", path, err)
 	}
 	return nil
+}
+
+func linearRegression(xys plotter.XYs, alpha float64) (m, c float64) {
+
+	for i := 0; i < iterations; i++ {
+		dm, dc := computeGradient(xys, m, c)
+		m += -dm * alpha
+		c += -dc * alpha
+		fmt.Printf("grad(%.2f, %.2f) = (%.2f, %.2f)\n", m, c, dm, dc)
+	}
+	fmt.Printf("cost(%.2f, %.2f) = %.2f\n", m, c, computeCost(xys, m, c))
+	return m, c
+}
+
+func computeCost(xys plotter.XYs, m, c float64) float64 {
+	// cost = 1/N * sum((y-(m*x+c)^2)
+	s := 0.0
+
+	for _, xy := range xys {
+		d := xy.Y - (xy.X*m + c)
+		s += d * d
+	}
+
+	return s / float64(len(xys))
+}
+
+func computeGradient(xys plotter.XYs, m, c float64) (dm, dc float64) {
+	// cost = 1/N * sum((y-(m*x+c)^2)
+
+	// d cost / dm = d/dt(1/N * sum((y-(m*x+c)^2))
+	// d cost / dm = 2/N * sum(-x(y-(m*x+c)))
+
+	// d cost / dc = d/dc(1/N * sum((y-(m*x+c)^2))
+	// d cost / dc = 2/N * sum(-(y-(m*x+c)))
+
+	for _, xy := range xys {
+		d := xy.Y - (xy.X*m + c)
+		dm += -xy.X * d
+		dc += -d
+	}
+
+	return 2 / float64(len(xys)) * dm, 2 / float64(len(xys)) * dc
 }
